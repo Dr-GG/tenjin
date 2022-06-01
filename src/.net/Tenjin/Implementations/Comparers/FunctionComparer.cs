54162facfee8c7d9dd1aaec744bcef2d7a2a3f06
Gerard Gouws <gerard.gouws@capgemini.com>
2022-06-01 10:57:21 +0200

    Added functions comparer
    20220601 - Added generic functions comparer with unit tests.

---
+++ src/.net/Tenjin/Implementations/Comparers/FunctionComparer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Tenjin.Implementations.Comparers
+{
+    public class FunctionComparer<TValue> : IComparer, IComparer<TValue>
+    {
+        private readonly Func<TValue, TValue, int> _functionComparer;
+
+        public FunctionComparer(Func<TValue, TValue, int> functionComparer)
+        {
+            _functionComparer = functionComparer;
+        }
+
+        public int Compare(object? x, object? y)
+        {
+            var result = PreCompare(x, y);
+
+            return result ?? _functionComparer((TValue)x!, (TValue) y!);
+        }
+
+        public int Compare(TValue? x, TValue? y)
+        {
+            var result = PreCompare(x, y);
+
+            return result ?? _functionComparer(x!, y!);
+        }
+
+        private static int? PreCompare(object? x, object? y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+
+            if (x == null && y != null)
+            {
+                return -1;
+            }
+
+            if (x != null && y == null)
+            {
+                return 1;
+            }
+
+            return null;
+        }
+    }
+}
